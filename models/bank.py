629c875056d1bc733d65b952b4c7970706d9c193
Jasemonu <jasemonu@gmail.com>
2023-12-30 14:55:42 +0000

    all

--- models/bank.py
+++ models/bank.py
@@ -1,8 +1,9 @@
 from mongoengine import Document, StringField, DateTimeField, ObjectIdField
+from mongoengine import ReferenceField
 from datetime import date
 
 class Bank(Document):
-    staff_number = StringField(required=True, max_length=70)
+    staff = ReferenceField('User', required=True, reverse_delete_rule=2)
     name = StringField(required=True, max_length=70, unique=True)
     branch = StringField(required=True, max_length=70)
     code = StringField(required=True, max_length=70)
